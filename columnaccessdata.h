12932054ab5de330bdfd6dcac30797fccc939409
pacmaninbw <paul.chernick@chernicksw.com>
2024-08-31 10:14:23 -0700

    Added TableIds enum. Moved table names out of columnaccessdata, but added TableIds to columnaccessdata. This allows table data to be modified without modifying columnaccessdata.

--- columnaccessdata.h
+++ columnaccessdata.h
@@ -2,6 +2,7 @@
 #define COLUMNACCESSDATA_H
 
 #include "columnidenum.h"
+#include "tableidenum.h"
 #include <initializer_list>
 #include <string>
 #include <vector>
@@ -10,27 +11,21 @@ class ColumnAccessDataMap
 {
 public:
     ColumnAccessDataMap(
-            ColumnIds id,
-            std::string nameIn,
-            std::string tableNameIn,
-            std::initializer_list<std::string>(columns)
+        ColumnIds id,
+        TableIds tableIdIn,
+        std::string nameIn,
+        std::initializer_list<std::string>(columns)
     );
-    void setPosition(unsigned int position) noexcept { positionInDisplay = position; }
-    void setEnabled(bool enabler) noexcept {enabled = enabler; };
-    unsigned int getPosition() const noexcept { return positionInDisplay; }
-    const ColumnIds getColumnId() const noexcept { return columnId; }
-    const bool getEnabled() const noexcept { return enabled; };
+    ColumnIds getColumnId() const noexcept { return columnId; }
+    TableIds getTableId() const noexcept { return tableID; }
     const std::string getName() const noexcept { return name; };
-    const std::string getTableName() const noexcept { return tableName; };
     const std::vector<std::string> getColumnsInTable() noexcept { return columnsInTable; };
 
 private:
-    unsigned int positionInDisplay;
-    std::string name;
-    std::string tableName;
-    std::vector<std::string> columnsInTable;
     ColumnIds columnId;
-    bool enabled;
+    TableIds tableID;
+    std::string name;
+    std::vector<std::string> columnsInTable;
 };
 
 #endif // COLUMNACCESSDATA_H
---
+++ tableidenum.h
@@ -0,0 +1,21 @@
+#ifndef TABLEIDENUM_H
+#define TABLEIDENUM_H
+
+enum class TableIds
+{
+    NO_TABLE,
+    TEMPURATURE,
+    REPIRATION_RATE,
+    BLOOD_PRESSURE,     // Currently Blood Pressure and Pulse Rate
+    BLOOD_OXYGEN,
+    WEIGHT,
+    BLOOD_SUGAR,
+    EXERCISE,
+    SLEEP,
+    BOWEL_MOVEMENTS,
+    NUTRITION,
+    NOTES,
+    LAST_TABLE_ID
+};
+
+#endif // TABLEIDENUM_H
