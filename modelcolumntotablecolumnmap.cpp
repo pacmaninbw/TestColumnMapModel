1f1906dcfa0fd97c24e8d89483a721417401831d
pacmaninbw <paul.chernick@chernicksw.com>
2024-08-31 10:27:22 -0700

    Added a separate struct for table data. Table data is now stored in a separate list accessable by TableIds enum. SQL queries are properly generated.  The SQL queries can be pasted into MySQL workbench and execute properly.

--- modelcolumntotablecolumnmap.cpp
+++ modelcolumntotablecolumnmap.cpp
@@ -1,193 +1,226 @@
 ﻿#include <algorithm>
+#include <string>
 #include <vector>
 
+#include "columnidenum.h"
+#include "tableidenum.h"
+#include "columnaccessdata.h"
+#include "dispalytodbtransferdata.h"
 #include "modelcolumntotablecolumnmap.h"
 #include "hdb_columnnames.h"
+
 #if (DEBUG || MY_UNIT_TEST)
 #include <iostream>
 #endif
 
+
 /*
- * Static basis for display column to database table and columnmapping.
+ * This data was originally stored in stat staticModelMap. This data is only needed
+ * when the SQL Query is generated so it was separated out. The TableIds enum was
+ * added to provide the link between the ColumnAccessDataMap object and the table
+ * names. The initialization of staticModelMap was becoming too complex.
+ */
+static std::vector<TableIdToTableName> tableNames =
+{
+    {TableIds::TEMPURATURE,     "Tempurature", "tmp" },
+    {TableIds::REPIRATION_RATE, "RepirationRate", "resp" },
+    {TableIds::BLOOD_PRESSURE,  "BloodPressure", "bp" },
+    {TableIds::BLOOD_OXYGEN,    "BloodOxygen", "bo" },
+    {TableIds::WEIGHT,          "Weight", "wt" },
+    {TableIds::BLOOD_SUGAR,     "BloodSugar", "bg" },
+    {TableIds::EXERCISE,        "Exercise", "exer" },
+    {TableIds::SLEEP,           "Sleep", "slp" },
+    {TableIds::BOWEL_MOVEMENTS, "BowlMovements", "bm" },
+    {TableIds::NUTRITION,       "Nutrition", "nut" },
+    {TableIds::NOTES,           "Notes", "nts" }
+};
+
+
+/*
+ * Static basis for display column to database table and column mapping.
  * Used for initialization of the model. All of the information to display
  * one column of data is in an element in the staticModelMap.
  */
 static std::vector<ColumnAccessDataMap> staticModelMap =
 {
-    // All columns are order by date.
-    {
-        ColumnIds::DATE,
-        "Date",
-        "Date_Primary_Indexing",
-        {"idDate_Primary_Indexing","Date_of_items"}
-    },
     // Vital Signs
     {
         ColumnIds::TEMPERATURE,
+        TableIds::TEMPURATURE,
         "Tempurature",
-        "No Table",
         {"No Columns", "Not Implemented"}
     },
     {
         ColumnIds::PUSLE_RATE,
+        TableIds::BLOOD_PRESSURE,
         "Pulse Rate",
-        "BloodPressure",
-        {"Pulse_Rate" "Not Implemented"}
+        {"Pulse_Rate"}
     },
     {
         ColumnIds::RESPIRATION_RATE,
+        TableIds::REPIRATION_RATE,
         "Respiration Rate",
-        "No Table",
         {"No Columns", "Not Implemented"}
     },
     {
         ColumnIds::BLOOD_PRESSURE,
+        TableIds::BLOOD_PRESSURE,
         "Blood Pressure",
-        "BloodPressure",
         {"Systolic","Diastolic"}
     },
     {
         ColumnIds::BLOOD_OXYGEN,
+        TableIds::BLOOD_OXYGEN,
         "Blood Oxygen",
-        "No Table",
-        {"No Columns", "Not Implemented"}
+        {"No Columns", "Not Implemented"},
     },
     {
         ColumnIds::WEIGHT,
-        "Weight",
+        TableIds::WEIGHT,
         "Weight",
         {"Weight_in_lbs"}
     },
     {
         ColumnIds::BLOOD_SUGAR,
+        TableIds::BLOOD_SUGAR,
         "Blood Glucose",
-        "Blood_Sugar",
         {"Blood_Sugar_Reading"}
     },
 
     // External Factors or Measurements
     {
         ColumnIds::EXERCISE,
-        "Exercise",
+        TableIds::EXERCISE,
         "Exercise",
         {"Exercise_Minutes"}
     },
     {
         ColumnIds::SLEEP_HOURS,
+        TableIds::SLEEP,
         "Sleep Hours",
-        "Sleep",
         {"Hours_of_Sleep",}
     },
     {
         ColumnIds::SLEEP_INTERRUPTIONS,
+        TableIds::SLEEP,
         "Sleep Interruptions",
-        "Sleep",
         {"Interrupts",}
     },
     {
         ColumnIds::BOWEL_MOVEMENTS,
-        "Bowel Movements",
-        "BowlMovements",
+        TableIds::BOWEL_MOVEMENTS,
+        "Bowl Movements",
         {"BM_Count"}
     },
     // Nutrition
     {
         ColumnIds::NUTRITION_CALORIES,
+        TableIds::NUTRITION,
         "Calories",
-        "Nutrition",
         {"Calories"}
     },
     {
         ColumnIds::NUTRITION_SATURATED_FAT,
+        TableIds::NUTRITION,
         "Saturated Fat",
-        "Nutrition",
         {"No Column", "Not Implemented"}
     },
     {
         ColumnIds::NUTRITION_TRANS_FAT,
-        "Saturated Fat",
-        "Nutrition",
+        TableIds::NUTRITION,
+        "Trans Fat",
         {"No Column", "Not Implemented"}
     },
     {
         ColumnIds::NUTRITION_CHOLESTEROL,
-        "Saturated Fat",
-        "Nutrition",
+        TableIds::NUTRITION,
+        "Cholesterol",
         {"No Column", "Not Implemented"}
     },
     {
         ColumnIds::NUTRITION_SODIUM,
+        TableIds::NUTRITION,
         "Sodium",
-        "Nutrition",
         {"Sodium_mgs"}
     },
     {
         ColumnIds::NUTRITION_FIBER,
+        TableIds::NUTRITION,
         "Fiber",
-        "Nutrition",
         {"Fiber_Grams"}
     },
     {
         ColumnIds::NUTRITION_TOTAL_SUGARS,
+        TableIds::NUTRITION,
         "Total Sugars",
-        "Nutrition",
         {"No Column", "Not Implemented"}
     },
     {
         ColumnIds::NUTRITION_PROTIEN,
+        TableIds::NUTRITION,
         "Protien",
-        "Nutrition",
         {"Protien_in_Grams"}
     },
-        {
+    {
         ColumnIds::NUTRITION_VITAMIN_D,
+        TableIds::NUTRITION,
         "Vitamin D",
-        "Nutrition",
         {"No Column", "Not Implemented"}
     },
     {
         ColumnIds::NUTRITION_CALCIUM,
+        TableIds::NUTRITION,
         "Calcium",
-        "Nutrition",
         {"No Column", "Not Implemented"}
     },
-        {
+    {
         ColumnIds::NUTRITION_IRON,
+        TableIds::NUTRITION,
         "Iron",
-        "Nutrition",
         {"No Column", "Not Implemented"}
     },
-        {
+    {
         ColumnIds::NUTRITION_POTASSIUM,
+        TableIds::NUTRITION,
         "Potassium",
-        "Nutrition",
         {"Potassium_mgs"}
     },
 
     // Other
     {
         ColumnIds::NOTES,
+        TableIds::NOTES,
         "Notes",
-        "Notes",
-        {"Note_Content"}
+        {"Note_Content"},
     }
 
 };
 
-ModelColumnToTableColumnMap::ModelColumnToTableColumnMap()
-{
-    for (auto newTableEntry: staticModelMap)
-    {
-        baseTable.push_back(newTableEntry);
-    }
-
-    DisplayToDBTransferData DateCol = {ColumnIds::DATE, 0 , true};
-    enableAndAddtoList(DateCol);
-}
-
 const std::string ModelColumnToTableColumnMap::buildQueryString() noexcept
 {
-    std::string sqlQuery("select ");
+    /*
+     * Sort the list to the order requested by the user.
+     */
+    std::sort(enabledList.begin(), enabledList.end(),
+        [](DisplayToDBTransferData a, DisplayToDBTransferData b)
+            {return  a.position < b.position;});
+
+    /*
+     * Generate an SQL query from the selected columns.
+     */
+    std::string selectedColumns("dpi.Date_of_items");
+    std::vector<TableIds> tablesToJoin;
+
+    for (auto columnData: enabledList)
+    {
+        selectedColumns += ",\n";
+        selectedColumns += addColumn(columnData.columnId);
+        addTableToJoinList(tablesToJoin, getColumnData(columnData.columnId).getTableId());
+    }
+
+    std::string sqlQuery("SELECT " + selectedColumns +
+        +"\nFROM Date_Primary_Indexing AS dpi\n " + buildJoinList(tablesToJoin) +
+        "ORDER BY dpi.Date_of_items ASC;");
 
     return sqlQuery;
 }
@@ -195,9 +228,8 @@ const std::string ModelColumnToTableColumnMap::buildQueryString() noexcept
 void ModelColumnToTableColumnMap::resetEnabledColumns()
 {
     enabledList.clear();
-    DisplayToDBTransferData DateCol = {ColumnIds::DATE, 0 , true};
-    enableAndAddtoList(DateCol);
 }
+
 
 void ModelColumnToTableColumnMap::enableUsedColumns(std::vector<DisplayToDBTransferData> dialogInput) noexcept
 {
@@ -216,6 +248,14 @@ void ModelColumnToTableColumnMap::enableAndAddtoList(DisplayToDBTransferData ena
 {
     ColumnIds columnToFind = enabledColumn.columnId;
 
+    // Don't assume the input data is correct, only add legitamate columns.
+    auto baseSource = std::find_if(staticModelMap.begin(), staticModelMap.end(),
+          [&columnToFind](ColumnAccessDataMap &cdm) {return (cdm.getColumnId() == columnToFind);});
+    if (baseSource == staticModelMap.end())
+    {
+        return;
+    }
+
     // No duplicate columns in the enabled list of columns.
     // Date is the zero element and will always be there.
     if (enabledList.size() > 0)
@@ -226,16 +266,7 @@ void ModelColumnToTableColumnMap::enableAndAddtoList(DisplayToDBTransferData ena
         }
     }
 
-    // Create a copy of the object in the base table, add the additional data from the
-    // columns dialog and add the copy to the enabled columns list.
-    auto baseSource = std::find_if(baseTable.begin(), baseTable.end(),
-          [&columnToFind](ColumnAccessDataMap &cdm) {return (cdm.getColumnId() == columnToFind);});
-
-    ColumnAccessDataMap columnToBeAdded = *baseSource;
-    columnToBeAdded.setPosition(enabledColumn.position);
-    columnToBeAdded.setEnabled(enabledColumn.enabled);
-
-    enabledList.push_back(columnToBeAdded);
+    enabledList.push_back(enabledColumn);
 }
 
 bool ModelColumnToTableColumnMap::duplicateInEnabledList(DisplayToDBTransferData candidateColumn)
@@ -244,10 +275,118 @@ bool ModelColumnToTableColumnMap::duplicateInEnabledList(DisplayToDBTransferData
     unsigned int pos = candidateColumn.position;
 
     auto duplicate = std::find_if(enabledList.begin(), enabledList.end(),
-        [&id, &pos](ColumnAccessDataMap &cdm)
-            {return ((cdm.getColumnId() == id) || (cdm.getPosition() == pos));});
+        [&id, &pos](DisplayToDBTransferData &cdm)
+            {return ((cdm.columnId == id) || (cdm.position == pos));});
 
     return (duplicate != enabledList.end());
+}
+
+/*
+ * While some tables will have an entry for every day and could use INNER JOIN,
+ * use LEFT JOIN for all so that there are no order dependent issues.
+ */
+std::string ModelColumnToTableColumnMap::addJoin(TableIds tblId) noexcept
+{
+    TableIdToTableName tableData = findTableData(tblId);
+
+    std::string tableName(tableData.tableName);
+    std::string tablePrefix(tableData.abrevTableName);
+    std::string sqlJoinClause(" LEFT JOIN " + tableName + " AS " + tablePrefix);
+
+    sqlJoinClause += " ON " + tablePrefix + "." +
+        "fk_Date_Index = dpi.idDate_Primary_Indexing\n";
+
+    return sqlJoinClause;
+}
+
+/*
+ * In most cases the columns displayed by the contents of a ColumnAccessDataMap
+ * object will be a single column, but there are special cases where it will be
+ * more than on column.
+ */
+std::string ModelColumnToTableColumnMap::addColumn(ColumnIds columnId) noexcept
+{
+    ColumnAccessDataMap columnData = getColumnData(columnId);
+    std::string addedColumnsWithTableName;
+    std::string tablePrefix(getSqlColumnQueryPrefix(columnData) + ".");
+    bool needComma = false;
+
+    for (auto column: columnData.getColumnsInTable())
+    {
+        if (needComma)
+        {
+           addedColumnsWithTableName += ", ";
+        }
+        std::string columnToAdd(tablePrefix + column);
+        addedColumnsWithTableName += columnToAdd;
+        needComma = true;
+    }
+
+    return addedColumnsWithTableName;
+}
+
+void ModelColumnToTableColumnMap::addTableToJoinList(std::vector<TableIds> &tablesToJoin, TableIds newEntry) noexcept
+{
+    auto tabIdIter = std::find(tablesToJoin.begin(), tablesToJoin.end(), newEntry);
+    if (tabIdIter == tablesToJoin.end())
+    {
+        tablesToJoin.push_back(newEntry);
+    }
+}
+
+std::string ModelColumnToTableColumnMap::buildJoinList(std::vector<TableIds> tablesToJoin) noexcept
+{
+    std::string joinSqlStr;
+
+    for (auto aTable: tablesToJoin)
+    {
+        joinSqlStr += addJoin(aTable);
+    }
+
+    return joinSqlStr;
+}
+
+const ColumnAccessDataMap ModelColumnToTableColumnMap::getColumnData(ColumnIds columnId) const noexcept
+{
+    auto columnSource = std::find_if(staticModelMap.begin(), staticModelMap.end(),
+          [&columnId](ColumnAccessDataMap &cdm) {return (cdm.getColumnId() == columnId);});
+
+    if (columnSource == staticModelMap.end())
+    {
+        ColumnAccessDataMap badData(ColumnIds::NO_COLUMN, TableIds::NO_TABLE, "Bad Data", {"Bad Data"});
+        return badData;
+    }
+
+    return *columnSource;
+}
+
+const TableIdToTableName ModelColumnToTableColumnMap::findTableData(TableIds tableId) const noexcept
+{
+    TableIdToTableName badTable = {TableIds::NO_TABLE, "bad table", "bad"};
+
+    auto tblDataIterator = std::find_if(tableNames.begin(), tableNames.end(),
+        [&tableId](TableIdToTableName &tbd) {return (tbd.id == tableId); });
+    if (tblDataIterator == tableNames.end())
+    {
+        return badTable;
+    }
+
+    return *tblDataIterator;
+}
+
+const TableIdToTableName ModelColumnToTableColumnMap::findTableData(ColumnIds columnId) const noexcept
+{
+    TableIdToTableName badTable = {TableIds::NO_TABLE, "bad table", "bad"};
+
+    ColumnAccessDataMap columnData = getColumnData(columnId);
+    if (columnData.getColumnId() != ColumnIds::NO_COLUMN)
+    {
+        return findTableData(columnData.getTableId());
+    }
+    else
+    {
+        return badTable;
+    }
 }
 
 #if (DEBUG || MY_UNIT_TEST)
@@ -279,9 +418,7 @@ void ModelColumnToTableColumnMap::debugPrintListMember(ColumnAccessDataMap mapTo
     std::cout << "\tColumn ID converted to string " <<
         dictionary.convertToString(mapToPrint.getColumnId()) << "\n";
     std::cout << "\tColumn Name " << mapToPrint.getName() << "\n";
-    std::cout << "\tColumn Position " << mapToPrint.getPosition() << "\n";
-    std::cout << "\t" << ((mapToPrint.getEnabled()) ? "Enabled\n" :"Disabled\n");
-    std::cout << "\tTable Name: " << mapToPrint.getTableName() << "\n";
+    std::cout << "\tTable Name: " << getTableName(mapToPrint) << "\n";
 
      const std::vector<std::string> columnsInTable = mapToPrint.getColumnsInTable();
      std::cout << "\tColumns to access in specified table:\n";
@@ -300,7 +437,7 @@ void ModelColumnToTableColumnMap::debugPrintTransferData(DisplayToDBTransferData
     std::cout << "Debugging or Testing DisplayToDBTransferData\n";
     std::cout << "\tColumnId: " << dictionary.convertToString(transferData.columnId)  << "\n";
     std::cout << "\tPosition: " << transferData.position << "\n";
-    std::cout << "\Enabled: " << (transferData.enabled ? "True" : "False") << "\n";
+    std::cout << "\tEnabled: " << (transferData.enabled ? "True" : "False") << "\n";
 }
 #endif
 
--- modelcolumntotablecolumnmap.h
+++ modelcolumntotablecolumnmap.h
@@ -5,7 +5,14 @@
 #include <vector>
 #include "columnaccessdata.h"
 #include "dispalytodbtransferdata.h"
-#define MY_UNIT_TEST 1
+
+typedef struct TableIdToTableName
+{
+    TableIds id = TableIds::NO_TABLE;
+    std::string tableName;
+    std::string abrevTableName;
+}
+TableIdToTableName;
 
 /*
  * This class will be a component of a QT Table model either by aggregation
@@ -14,23 +21,54 @@
 class ModelColumnToTableColumnMap
 {
 public:
-    ModelColumnToTableColumnMap();
+    ModelColumnToTableColumnMap() = default;
     void enableUsedColumns(std::vector<DisplayToDBTransferData> dialogInput) noexcept;
     const std::string buildQueryString() noexcept;
-    const std::vector<ColumnAccessDataMap> getEnabledList() const noexcept { return enabledList; };
+    const std::vector<DisplayToDBTransferData> getEnabledList() const noexcept { return enabledList; };
+    std::size_t getEnabledCount() const noexcept { return enabledList.size(); };
     void resetEnabledColumns();
 
 #if (DEBUG || MY_UNIT_TEST)
-    void debugPrintEnabledList() { debugPrintList(enabledList); };
-    void debugPrintBaseTable() { debugPrintList(baseTable); };
+    void debugPrintEnabledList() { debugPrintTransferDataList(enabledList); };
     void debugPrintTransferDataList(std::vector<DisplayToDBTransferData> testList);
 #endif
 
 private:
     void enableAndAddtoList(DisplayToDBTransferData enabledColumn) noexcept;
     bool duplicateInEnabledList(DisplayToDBTransferData candidateColumn);
-    std::vector<ColumnAccessDataMap> baseTable;
-    std::vector<ColumnAccessDataMap> enabledList;
+    std::string addJoin(TableIds tblId) noexcept;
+    std::string addColumn(ColumnIds columnId) noexcept;
+    void addTableToJoinList(std::vector<TableIds> &tablesToJoin, TableIds newEntry) noexcept;
+    std::string buildJoinList(std::vector<TableIds> tablesToJoin) noexcept;
+    const ColumnAccessDataMap getColumnData(ColumnIds columnId) const noexcept;
+    const ColumnAccessDataMap getColumnData(
+            DisplayToDBTransferData transferData) const noexcept
+    { return getColumnData(transferData.columnId); }
+    ;
+    const TableIdToTableName findTableData(TableIds tableId) const noexcept;
+    const TableIdToTableName findTableData(ColumnIds columnId) const noexcept;
+    const TableIdToTableName findTableData(ColumnAccessDataMap columnData) const noexcept
+        { return findTableData(columnData.getTableId()); }
+    const TableIdToTableName findTableData(DisplayToDBTransferData transferData) const noexcept
+        { return findTableData(transferData.columnId); }
+    const std::string getTableName(TableIds tableId) const noexcept
+        { return findTableData(tableId).tableName; }
+    const std::string getTableName(ColumnIds columnId) const noexcept
+        { return findTableData(columnId).tableName; }
+    const std::string getTableName(ColumnAccessDataMap columnData) const noexcept
+        { return findTableData(columnData).tableName; }
+    const std::string getTableName(DisplayToDBTransferData transferData) const noexcept
+        { return findTableData(transferData).tableName; }
+    const std::string getSqlColumnQueryPrefix(TableIds tableId) const noexcept
+        { return findTableData(tableId).abrevTableName; }
+    const std::string getSqlColumnQueryPrefix(ColumnIds columnId) const noexcept
+        { return findTableData(columnId).abrevTableName; }
+    const std::string getSqlColumnQueryPrefix(ColumnAccessDataMap columnData) const noexcept
+        { return findTableData(columnData).abrevTableName; }
+    const std::string getSqlColumnQueryPrefix(DisplayToDBTransferData transferData) const noexcept
+        { return findTableData(transferData).abrevTableName; }
+
+    std::vector<DisplayToDBTransferData> enabledList;
 
 #if (DEBUG || MY_UNIT_TEST)
     void debugPrintList(std::vector<ColumnAccessDataMap> listToPrint);
